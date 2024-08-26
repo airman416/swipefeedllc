1ca5759d93e5d5caae07652f13a56b1a48e8d3e3
Armaan Agrawal <im.armaan.agrawal@gmail.com>
2024-08-26 15:39:36 -0400

    SwipeFeed

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        © SwipeFeed LLC 2024
       </footer>
     </>
   )
