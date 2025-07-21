e740bad264fd5222049a4eb42473d9db552bed81
Armaan Agrawal <agrawal.arm@northeastern.edu>
2025-07-21 15:59:41 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export const metadata = {
   title: 'SwipeFeed',
   description: 'Share and discover stories on SwipeFeed',
@@ -6,7 +8,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BVJ7ZEVTRJ"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BVJ7ZEVTRJ');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 } 
